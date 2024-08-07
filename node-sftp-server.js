87268d369dbfdb850867b6ec9f6f32ab5daf16c1
Vladimir Lomonos <vladimir.lomonos@finsight.com>
2024-08-07 12:39:30 +0300

    Node SFTP server debug, FIN-23155

--- node-sftp-server.js
+++ node-sftp-server.js
@@ -418,10 +418,7 @@ var SFTPSession = (function(superClass) {
 					tmpPath: tmpPath,
 					tmpFile: fd
 				};
-        var writestream = new Writable();
-        writestream._write = () => {
-        };
-				// var writestream = fs.createWriteStream(tmpPath);
+				var writestream = fs.createWriteStream(tmpPath);
 				writestream.on("finish", function() {
 					this.handles[handle].finished = true;
 				}.bind(this));
